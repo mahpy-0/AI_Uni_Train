636d4a49a190ec6529227e1c0876f11df55afb6e
mahpy <mohammadmahditeymourisichani@gmail.com>
2025-03-12 19:48:18 +0330

    classified environment, implemented main but not tested

--- Environment.py
+++ Environment.py
@@ -1,7 +1,9 @@
-from static_info import (
-    ENV,
-    AGENT_LOC
-)
+# from static_info import (
+#     ENV,
+#     AGENT_LOC
+# )
+
+import random
 
 # region function_base
 
@@ -43,7 +45,6 @@ from static_info import (
 #         ]
 
 #     elif action == "go_left":
-
 #         AGENT_LOC[1] -= 1
 #         return [
 #             AGENT_LOC[0],
@@ -62,9 +63,112 @@ from static_info import (
 
 # region class_base
 
-
 class Environment():
     def __init__(self):
-        pass
+        self._ENV = None
+        self._DIMENSIONS = None
+        self._agent_location = None
+
+        self._rows = None
+        self._columns = None
+
+    def create_ENV(self):
+
+        rows = random.randint(1, 10)
+        columns = random.randint(1, 10)
+
+        tiles = []
+        for row in range(rows):
+            row_numbers = []
+            for column in range(columns):
+                row_numbers.append(random.randint(0, 1))
+            tiles.append(row_numbers)
+
+        self._ENV = tiles.copy
+        self._DIMENSIONS = (rows, columns)
+        self._rows = rows
+        self._columns = columns
+
+    def start_agent_loc(self):
+        agent_row = random.randint(0, self._rows - 1)
+        agent_column = random.randint(0, self._columns - 1)
+        self._agent_location = [agent_row, agent_column]
+
+    def get_rows(self):
+        return self._rows
+
+    def get_columns(self):
+        return self._columns
+
+    def get_ENV(self):
+        return self._ENV
+
+    def print_state(self):
+        for row in range(self._rows):
+            for column in range(self._columns):
+                print(self._ENV[row][column], end= "\t")
+            print()
+
+    def think(self, action: str) -> list:
+
+        """
+        take a action
+
+        action =: suck | go_right | go_left | go_up | go_down | start
+
+        return precept as list for agent
+
+        precept: 
+            [0] := agent row
+            [1] := agent column
+            [2] := environment state
+        """
+
+        if action == "suck":
+            self._ENV[self._agent_location[0]][self._agent_location[1]] = 0
+            return [
+                self._agent_location[0],
+                self._agent_location[1],
+                self._ENV[self._agent_location[0]][self._agent_location[1]]
+            ]
+
+        elif action == "go_down":
+            self._agent_location[0] += 1
+            return [
+                self._agent_location[0],
+                self._agent_location[1],
+                self._ENV[self._agent_location[0]][self._agent_location[1]]
+            ]
+
+        elif action == "go_up":
+            self._agent_location[0] -= 1
+            return [
+                self._agent_location[0],
+                self._agent_location[1],
+                self._ENV[self._agent_location[0]][self._agent_location[1]]
+            ]
+
+        elif action == "go_right":
+            self._agent_location[1] += 1
+            return [
+                self._agent_location[0],
+                self._agent_location[1],
+                self._ENV[self._agent_location[0]][self._agent_location[1]]
+            ]
+
+        elif action == "go_left":
+            self._agent_location[1] -= 1
+            return [
+                self._agent_location[0],
+                self._agent_location[1],
+                self._ENV[self._agent_location[0]][self._agent_location[1]]
+            ]
+
+        elif action == "start":
+            return [
+                self._agent_location[0],
+                self._agent_location[1],
+                self._ENV[self._agent_location[0]][self._agent_location[1]]
+            ]
 
 # endregion
--- main.py
+++ main.py
@@ -1,37 +1,74 @@
-from static_info import (
-    FULL_CLEAN_FLAG,
-    ENV,
-    rows,
-    columns
-)
-from Agent import agent
-from Environment import environment
+# from static_info import (
+#     FULL_CLEAN_FLAG,
+#     ENV,
+#     rows,
+#     columns
+# )
+# from Agent import agent
+# from Environment import environment
+
+
+# if __name__ == "__main__":
+
+    # print(f"rows: {rows}, columns: {columns}")
+
+    # for row in range(rows):
+    #     for column in range(columns):
+    #         print(ENV[row][column], end= "\t")
+    #     print()
+
+    # precept = environment("start")
+    # action = agent(precept)
+
+    # if action == "suck":
+    #     print("cleaning")
+
+    # while not FULL_CLEAN_FLAG:
+    #     precept = environment(action)
+    #     action = agent(precept)
+    #     if action == "suck":
+    #         print("cleaning")
+
+    # for row in range(rows):
+    #     for column in range(columns):
+    #         print(ENV[row][column], end= "\t")
+    #     print()
+
+# print("done")
+
+
+from Environment import Environment
+from Agent import Agent
+from static_info import FULL_CLEAN_FLAG
 
 
 if __name__ == "__main__":
 
+    environment = Environment()
+
+    environment.create_ENV()
+    environment.start_agent_loc()
+
+    agent = Agent(environment.get_ENV())
+
+    rows = environment.get_rows()
+    columns = environment.get_columns()
+
     print(f"rows: {rows}, columns: {columns}")
 
-    for row in range(rows):
-        for column in range(columns):
-            print(ENV[row][column], end= "\t")
-        print()
+    environment.print_state()
 
-    precept = environment("start")
-    action = agent(precept)
-
-    if action == "suck":
-        print("cleaning")
+    precept = environment.think("start")
+    action = agent.think(precept)
 
     while not FULL_CLEAN_FLAG:
-        precept = environment(action)
-        action = agent(precept)
+
         if action == "suck":
             print("cleaning")
-
-    for row in range(rows):
-        for column in range(columns):
-            print(ENV[row][column], end= "\t")
-        print()
+        
+        precept = environment.think(action)
+        action = agent.think(precept)
+    
+    environment.print_state()
 
     print("done")
